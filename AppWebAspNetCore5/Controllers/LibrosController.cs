3ebff5ca93f7e85e8cacb63988e7a49bd964cbb4
Jhon Sebastian Giraldo Montoya <sebastiangiraldo0917@gmail.com>
2021-08-08 23:52:28 -0500

    Agregar archivos de proyecto.

---
+++ AppWebAspNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,110 @@
+﻿using AppWebAspNetCore5.Data;
+using AppWebAspNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWebAspNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listLibros = _context.Libro;
+            return View(listLibros);
+        }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+                TempData["mensaje"] = "El libro se ha creado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id== null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+
+            }
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+                TempData["mensaje"] = "El libro se ha modificado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+
+            }
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            var libro = _context.Libro.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+            TempData["mensaje"] = "El libro se ha Eliminado correctamente";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AppWebAspNetCore5/Models/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWebAspNetCore5.Models
+{
+    public class Libro
+    {[Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Titulo Es Obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "TITULO")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "El Descripcion Es Obligatoria")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "DESCRIPCION")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La Fecha de Lanzamiento es obligatoria")]
+        [DataType(DataType.Date)]
+        [Display(Name = "fecha lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+        [Required(ErrorMessage = "El Autor es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Autor")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "El Precio es obligatorio")]
+        public int Precio { get; set; }
+    }
+}
